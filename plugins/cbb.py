c661305c5b7f30aa183012c8f8556c8d86707cec
TG-BOTZ <88578997+TG-BOTz@users.noreply.github.com>
2021-08-14 09:59:10 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>SMILE KILLER</a>\n○ GROUP :  <a href='https://t.me/FILMCORNERMM/'>CLICK HERE</a>\n○ GROUP : <a href='https://t.me/NAZRIYAOFFTOPIC/'>CLICK HERE</a>\n○ CHANNEL : <a href='https://t.me/FILM_CORNER_OFFICIAL/'>CLICK HERE</a>\n○ CHANNAL : <a href='https://t.me/NAZRIYAONTOPIC/'>CLICK HERE</a>\n○ GROUP : <a href='https://t.me/NAZRIYASUPPORT/'>CLICK HERE</a></b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>SMILE KILLER</a>\n○ GROUP :  <a href='https://t.me/FILMCORNERMM/'>CLICK HERE</a>\n○ GROUP : <a href='https://t.me/NAZRIYAOFFTOPIC/'>CLICK HERE</a>\n○ GROUP : <a href='https://t.me/NAZRIYASUPPORT/'>CLICK HERE</a>\n○ CHANNAL : <a href='https://t.me/NAZRIYAONTOPIC/'>CLICK HERE</a>\n○ CHANNEL : <a href='https://t.me/FILM_CORNER_OFFICIAL/'>CLICK HERE</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
